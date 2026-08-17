95e19b50f9645c3bc8ce74b28e319aeccebadf1a
Eetu Mäenpää <me@eetumaenpaa.fi>
2026-08-17 13:35:27 +0300

    Exercise 1.7

--- log_output/index.js
+++ log_output/index.js
@@ -1,6 +1,20 @@
+import { createServer } from "node:http"
 import { randomUUID } from "node:crypto"
 
+const port = process.env.PORT ?? 3000
 const randomString = randomUUID()
+
+const server = createServer((req, res) => {
+    res.writeHead(200, {
+        "content-type": "application/json"
+    })
+    res.end(JSON.stringify({
+        timestamp: new Date().toISOString(),
+        randomString
+    }))
+})
+
+server.listen(port)
 
 while (true) {
     const timestamp = new Date().toISOString()
