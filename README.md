19d8aaefdbcf1840d20cdfad6a24e4b9c3aeb7ac
Eetu Mäenpää <me@eetumaenpaa.fi>
2026-08-31 08:31:21 +0300

    Exercise 2.2

--- README.md
+++ README.md
@@ -18,3 +18,4 @@
 - [1.12.](https://github.com/Eldemarkki/devops-with-kubernetes-submissions/tree/1.12/the_project/)
 - [1.13.](https://github.com/Eldemarkki/devops-with-kubernetes-submissions/tree/1.13/the_project/)
 - [2.1.](https://github.com/Eldemarkki/devops-with-kubernetes-submissions/tree/2.1/ping-pong-app/)
+- [2.2.](https://github.com/Eldemarkki/devops-with-kubernetes-submissions/tree/2.2/todo-backend/)
---
+++ todo-backend/index.js
@@ -0,0 +1,61 @@
+// @ts-check
+
+import { fastify } from "fastify"
+
+const port = process.env.PORT
+if (!port) throw new Error("Please specify a port")
+
+const app = fastify()
+
+/** @typedef {{ id: string, text: string }} Todo */
+
+/** @type {Todo[]} */
+const todos = []
+
+/**
+ * @param {unknown} value
+ * @returns {value is Todo}
+ */
+function isTodo(value) {
+    return typeof value === "object"
+        && value !== null
+        && "id" in value
+        && typeof value.id === "string"
+        && "text" in value
+        && typeof value.text === "string"
+}
+
+app.get("/todos", async (req, res) => {
+    res.send(todos)
+})
+
+app.post("/todos", async (req, res) => {
+    if (!isTodo(req.body)) {
+        return res.status(400).send({ error: "Invalid todo" })
+    }
+
+    const data = req.body
+
+    todos.push(data)
+    return res.status(201).send()
+})
+
+async function shutdown() {
+    try {
+        await app.close()
+    }
+    catch (error) {
+        console.error(error)
+        process.exitCode = 1
+    }
+}
+
+process.once("SIGTERM", () => shutdown())
+process.once("SIGINT", () => shutdown())
+
+app.listen({
+    port: parseInt(port, 10),
+    host: "0.0.0.0"
+}, () => {
+    console.log(`Server started in port ${port}`)
+})
